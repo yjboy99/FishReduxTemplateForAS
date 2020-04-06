e764ff8ddc3b25e2ebf3b5b62a45c699bf7a98ef
dIzsxXurIL <yjboy99@163.com>
2020-04-06 22:14:05 +0800

    添加常用

--- src/templates/state.dart
+++ src/templates/state.dart
@@ -1,5 +1,5 @@
 import 'package:fish_redux/fish_redux.dart';
-
+//import '../../state.dart';
 class $nameState implements Cloneable<$nameState> {
 
   @override
@@ -11,3 +11,9 @@ class $nameState implements Cloneable<$nameState> {
 $nameState initState(Map<String, dynamic> args) {
   return $nameState();
 }
+//class $nameConnector extends ConnOp<DetailsState,$nameState>{
+//  @override
+//  $nameState get(DetailsState state) {
+//    return $nameState();
+//  }
+//}
